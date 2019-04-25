bce24a3af3e771cedace52085c935f562d86514a
2052550012@qq.com <757521274mq>
2019-04-25 23:48:55 +0800

    eureka

--- consume-demo/src/main/java/priv/kira/springcloudtest/consumedemo/controller/EurekaTestController.java
+++ consume-demo/src/main/java/priv/kira/springcloudtest/consumedemo/controller/EurekaTestController.java
@@ -6,12 +6,16 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/consume")
+@RequestMapping("/consume/eurekaclient")
 public class EurekaTestController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
     private DiscoveryClient discoveryClient;
@@ -24,10 +28,9 @@ public class EurekaTestController {
 
     @RequestMapping("/userinfo/{id}")
     public String getuserinfo(@PathVariable("id") Long id){
-        String userinfojson="";
-
-
-
-        return userinfojson;
+        List<ServiceInstance> list = discoveryClient.getInstances("user-service");
+        ServiceInstance serviceInstance=list.get(0);
+        //String url="http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+"/user/"+id;
+        return restTemplate.getForObject(serviceInstance.getUri()+"/user/"+id,String.class);
     }
 }
